658f18f5c48ac6a2d6a2e6151d2f49a0f81c50b6
nhacnt17 <nhac17122003@gmail.com>
2024-12-09 00:25:53 +0700

    add Fonts

---
+++ src/constants/appFonts.ts
@@ -0,0 +1,7 @@
+export const appFonts = {
+    Bold: 'Montserrat-Bold',
+    Medium: 'Montserrat-Medium',
+    Light: 'Montserrat-Light',
+    SemiBold: 'Montserrat-SemiBold',
+    Regular: 'Montserrat-Regular'
+}
